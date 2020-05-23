79ffe5ab42bdc5af073f7bdcaafed7ccc2e2e223
Tanya Obraztsova <t.obraztsova@owox.com>
2020-05-23 16:41:54 +0300

    Added checkbox

--- src/app/todo-item/todo-item.component.ts
+++ src/app/todo-item/todo-item.component.ts
@@ -9,10 +9,18 @@ import { TodoItem } from '../interfaces/todo-item';
 export class TodoItemComponent implements OnInit {
   @Input() item: TodoItem = {};
   @Output() remove: EventEmitter<TodoItem> = new EventEmitter();
+  @Output() update: EventEmitter<any> = new EventEmitter();
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  completeItem() {
+    this.update.emit({
+      item: this.item,
+      changes: { completed: !this.item.completed }
+    });
+  }
 
   removeItem() {
     this.remove.emit(this.item);
